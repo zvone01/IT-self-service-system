3e1256b32751608d984193474588127b6fca192b
zdujmic <zdujmic@HP-ZDUJMIC>
2017-12-20 14:55:14 +0100

    aa

---
+++ IT self service system/IT self service system/Migrations/201712201354429_fromNewTest.cs
@@ -0,0 +1,17 @@
+namespace IT_self_service_system.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fromNewTest : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
